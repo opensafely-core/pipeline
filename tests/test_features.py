a77c3927f6a7a7ef4d9383eb806c3221aaf015dd
Alice Wong <alice.wong@thedatalab.org>
2024-10-03 18:07:32 +0100

    change LATEST_VERSION to 4

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pipeline.features import get_feature_flags_for_version
 
 
@@ -6,6 +8,7 @@ def test_get_feature_flags_for_version_with_v1():
 
     assert not flags.UNIQUE_OUTPUT_PATH
     assert not flags.EXPECTATIONS_POPULATION
+    assert not flags.REMOVE_SUPPORT_FOR_COHORT_EXTRACTOR
 
 
 def test_get_feature_flags_for_version_with_v2():
@@ -13,6 +16,7 @@ def test_get_feature_flags_for_version_with_v2():
 
     assert flags.UNIQUE_OUTPUT_PATH
     assert not flags.EXPECTATIONS_POPULATION
+    assert not flags.REMOVE_SUPPORT_FOR_COHORT_EXTRACTOR
 
 
 def test_get_feature_flags_for_version_with_v3():
@@ -20,3 +24,18 @@ def test_get_feature_flags_for_version_with_v3():
 
     assert flags.UNIQUE_OUTPUT_PATH
     assert flags.EXPECTATIONS_POPULATION
+    assert not flags.REMOVE_SUPPORT_FOR_COHORT_EXTRACTOR
+
+
+def test_get_feature_flags_for_version_with_v4():
+    flags = get_feature_flags_for_version(4)
+
+    assert flags.UNIQUE_OUTPUT_PATH
+    assert not flags.EXPECTATIONS_POPULATION
+    assert flags.REMOVE_SUPPORT_FOR_COHORT_EXTRACTOR
+
+
+def test_get_feature_flags_for_version_with_v5():
+    msg = "The latest version is v4, but got v5"
+    with pytest.raises(ValueError, match=msg):
+        get_feature_flags_for_version(5)
