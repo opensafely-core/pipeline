a14b7cd06fd28652ae9320906d1caad56d7ec0b2
Becky Smith <becky.smith@thedatalab.org>
2026-08-21 10:07:33 +0100

    Remove expectations
    We now enforce minimum version 4, which doesn't allow running
    cohort-extractor actions, which are the actions that required
    an expectations section. cohort-extractor is now deprecated and
    can't be re-run anyway, so get rid of the Expectations and its
    associated validation altogether.

--- tests/test_type_validation.py
+++ tests/test_type_validation.py
@@ -10,21 +10,14 @@ def test_missing_actions():
     with pytest.raises(
         ValidationError, match="Project `actions` section must be a dictionary"
     ):
-        Pipeline.build(version=3, expectations={"population_size": 10})
+        Pipeline.build(version=4)
 
 
 def test_actions_incorrect_type():
     with pytest.raises(
         ValidationError, match="Project `actions` section must be a dictionary"
     ):
-        Pipeline.build(version=3, actions=[], expectations={"population_size": 10})
-
-
-def test_expectations_incorrect_type():
-    with pytest.raises(
-        ValidationError, match="Project `expectations` section must be a dictionary"
-    ):
-        Pipeline.build(version=3, actions={}, expectations=[])
+        Pipeline.build(version=4, actions=[])
 
 
 def test_outputs_incorrect_type():
@@ -33,9 +26,8 @@ def test_outputs_incorrect_type():
         match="`outputs` section for action action1 must be a dictionary",
     ):
         Pipeline.build(
-            version=3,
+            version=4,
             actions={"action1": {"outputs": [], "run": "test:v1"}},
-            expectations={"population_size": 10},
         )
 
 
@@ -44,9 +36,8 @@ def test_run_incorrect_type():
         ValidationError, match="`run` section for action action1 must be a string"
     ):
         Pipeline.build(
-            version=3,
+            version=4,
             actions={"action1": {"outputs": {}, "run": ["test:v1"]}},
-            expectations={"population_size": 10},
         )
 
 
@@ -55,9 +46,8 @@ def test_needs_incorrect_type():
         ValidationError, match="`needs` section for action action1 must be a list"
     ):
         Pipeline.build(
-            version=3,
+            version=4,
             actions={"action1": {"outputs": {}, "run": "test:v1", "needs": ""}},
-            expectations={"population_size": 10},
         )
 
 
@@ -67,9 +57,8 @@ def test_config_incorrect_type():
         match="`config` section for action action1 must be a dictionary",
     ):
         Pipeline.build(
-            version=3,
+            version=4,
             actions={"action1": {"outputs": {}, "run": "test:v1", "config": []}},
-            expectations={"population_size": 10},
         )
 
 
@@ -79,11 +68,10 @@ def test_dummy_data_file_incorrect_type():
         match="`dummy_data_file` section for action action1 must be a string",
     ):
         Pipeline.build(
-            version=3,
+            version=4,
             actions={
                 "action1": {"outputs": {}, "run": "test:v1", "dummy_data_file": []}
             },
-            expectations={"population_size": 10},
         )
 
 
@@ -93,11 +81,10 @@ def test_output_files_incorrect_type():
         match="`highly_sensitive` section for action action1 must be a dictionary",
     ):
         Pipeline.build(
-            version=3,
+            version=4,
             actions={
                 "action1": {"outputs": {"highly_sensitive": []}, "run": "test:v1"}
             },
-            expectations={"population_size": 10},
         )
 
 
@@ -107,14 +94,13 @@ def test_output_filename_incorrect_type():
         match="`dataset` output for action action1 must be a string",
     ):
         Pipeline.build(
-            version=3,
+            version=4,
             actions={
                 "action1": {
                     "outputs": {"highly_sensitive": {"dataset": {}}},
                     "run": "test:v1",
                 }
             },
-            expectations={"population_size": 10},
         )
 
 
@@ -131,7 +117,7 @@ def test_action_extra_parameters():
         match=re.escape("Unexpected parameters (extra) in action action1"),
     ):
         Pipeline.build(
-            version=3,
+            version=4,
             actions={
                 "action1": {
                     "outputs": {},
@@ -139,7 +125,6 @@ def test_action_extra_parameters():
                     "extra": 123,
                 }
             },
-            expectations={"population_size": 10},
         )
 
 
@@ -151,26 +136,11 @@ def test_outputs_extra_parameters():
         ),
     ):
         Pipeline.build(
-            version=3,
+            version=4,
             actions={
                 "action1": {
                     "outputs": {"highly_sensitive": {"dataset": {}}, "extra": 123},
                     "run": "test:v1",
                 }
             },
-            expectations={"population_size": 10},
-        )
-
-
-def test_expectations_extra_parameters():
-    with pytest.raises(
-        ValidationError,
-        match=re.escape(
-            "Unexpected parameters (extra) in project `expectations` section"
-        ),
-    ):
-        Pipeline.build(
-            version=3,
-            actions={},
-            expectations={"population_size": 10, "extra": 123},
         )
